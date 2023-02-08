2ef2a239d28dbea1565477674fe1dc5b2e92c067
Karina Radzevich <karina_radzevich@bk.ru>
2023-02-08 20:43:13 +0400

    task25 done with 2 ways but with a question about UppercaseLetter

---
+++ task25/Program.cs
@@ -0,0 +1,45 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+//3⁵ {a}{ᵇ} эта часть здесь выглядит как надо,а в консоле не вышло
+//пыталась по-разному,буду благодарна,если подскажете,как это правильно делать)
+// char symbol = 'B';
+// Console.WriteLine(char.GetUnicodeCategory(symbol));
+//а после этого варианта в консоли пишет UppercaseLetter
+
+//старый способ
+// Console.WriteLine("Введите 2 разных числа(А и В),чтобы увидеть число А в натуральной степени числа В");
+// Console.WriteLine("Введите число А: ");
+// int a = int.Parse(Console.ReadLine());
+// Console.WriteLine("Введите число В: ");
+// int b = int.Parse(Console.ReadLine());
+// if (a <= 0 || b <= 0)
+// {
+//     Console.WriteLine("Оба числа должны быть положительными,попробуйте снова");
+// }
+// else if (a > 0 && b > 0)
+// {
+//     Console.WriteLine($"{a}, {b} -> {Math.Pow(a, b)}");
+// }
+
+//новый способ
+Console.WriteLine("Введите 2 разных числа(А и В),чтобы увидеть число А в натуральной степени числа В");
+Console.WriteLine("Введите число А: ");
+int a = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число В: ");
+int b = int.Parse(Console.ReadLine());
+Console.WriteLine($"{a}, {b} -> {GetSomePow(a)}");
+if (a <= 0 || b <= 0)
+{
+    Console.WriteLine("Оба числа должны быть положительными,попробуйте снова");
+}
+double GetSomePow(double AB)
+{
+    for (int i = 0; i < b; i++)
+    {
+        AB = Math.Pow(a, b);
+    }
+    return AB;
+}
